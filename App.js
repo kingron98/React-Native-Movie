f7a062d3342e7daf130fe55699c6cca0359208a8
kingron98 <aaronfu123@gmail.com>
2021-01-11 15:52:05 +0800

    Second commit

--- App.js
+++ App.js
@@ -1,34 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from 'react';
-import moviesData from './moviesData';
 import Movies from './Movies';
 import Header from './components/Header';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView } from 'react-native';
 
-const moviesComponent = moviesData.map(item => <Movies key={item.imdbID} movies={item}/> )
-
 export default function App() {
 
-const [isLoading, setLoading] = useState(true);
-const [data, setData] = useState([]);
-console.log(data);
-console.log(setData);
-useEffect(() => {
-    fetch('http://www.omdbapi.com/?s=superman&apikey=28f4dae9')
-      .then((response) => response.json())
-      .then((json) => setData(json))
-      .catch((error) => console.error(error))
-      .finally(() => setLoading(false));
-  }, []);
+    const [isLoading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+    console.log(data);
 
-  return (
-<SafeAreaView>
-    <ScrollView className="App">
-      <Header />
-      {moviesComponent}
-    </ScrollView>
-</SafeAreaView>
-  );
+    useEffect(() => {
+        fetch('http://www.omdbapi.com/?s=superman&apikey=28f4dae9')
+        .then((response) => response.json())
+        .then((json) => setData(json ? json.Search : []))
+        .catch((error) => console.error(error))
+        .finally(() => setLoading(false));
+    }, []);
+
+    const moviesComponent = data.map(item => <Movies key={item.imdbID} movies={item}/> )
+    console.log(moviesComponent);
+
+    return (
+    <SafeAreaView>
+        <ScrollView className="App">
+            <Header />
+            {moviesComponent}
+        </ScrollView>
+    </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
